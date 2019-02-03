7c7dfb596d89032dc98ff2326fe010bd170482ae
Parth Sharma <parthrai2@gmail.com>
2019-02-03 14:43:15 -0500

    user page fixed

--- resources/views/admin/userPage.blade.php
+++ resources/views/admin/userPage.blade.php
@@ -66,20 +66,186 @@
                             <thead>
                             <tr>
                                 <th>Video</th>
-
+                                <th>Status</th>
                                 <th>Skip</th>
                             </tr>
                             </thead>
                             <tbody>
 
-                            @for($i=1; $i<=12 ; $i++)
+
                              <tr>
-                                <td>Video {{$i}}</td>
+                                <td>Video 1</td>
+                                <td>
+                                    @if($videoStatus->vid1)
+                                        completed
+                                    @else
+                                        not completed
+                                    @endif
 
-                                <td><a href="/admin/user/{{$user->id}}/video/{{$i}}" class="btn btn-primary">Skip</a></td>
-                            </tr>
+                                </td>
 
-                            @endfor
+                                <td><a href="/admin/user/{{$user->id}}/video/1" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 2</td>
+                                 <td>
+                                     @if($videoStatus->vid2)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/2" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 3</td>
+                                 <td>
+                                     @if($videoStatus->vid3)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/3" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+
+                             <tr>
+                                 <td>Video 4</td>
+                                 <td>
+                                     @if($videoStatus->vid4)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/4" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+
+                             <tr>
+                                 <td>Video 5</td>
+                                 <td>
+                                     @if($videoStatus->vid5)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/5" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+
+                             <tr>
+                                 <td>Video 6</td>
+                                 <td>
+                                     @if($videoStatus->vid6)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/6" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+
+                             <tr>
+                                 <td>Video 7</td>
+                                 <td>
+                                     @if($videoStatus->vid7)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/7" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 8</td>
+                                 <td>
+                                     @if($videoStatus->vid8)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/8" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 9</td>
+                                 <td>
+                                     @if($videoStatus->vid9)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/9" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 10</td>
+                                 <td>
+                                     @if($videoStatus->vid10)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/10" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 11</td>
+                                 <td>
+                                     @if($videoStatus->vid11)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/11" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+                             <tr>
+                                 <td>Video 12</td>
+                                 <td>
+                                     @if($videoStatus->vid12)
+                                         completed
+                                     @else
+                                         not completed
+                                     @endif
+
+                                 </td>
+
+                                 <td><a href="/admin/user/{{$user->id}}/video/12" class="btn btn-primary">Skip</a></td>
+                             </tr>
+
+
 
                             </tbody>
                         </table>
